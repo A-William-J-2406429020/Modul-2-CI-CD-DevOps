c9e2670a8eb826f3249414660e67957cfb0823d2
Weeleeam <william.jonnatan@ui.ac.id>
2026-02-08 21:19:53 +0700

    Add Validation

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -25,6 +25,11 @@ public class ProductController {
 
     @PostMapping("/create")
     public String createProductPost(@ModelAttribute Product product, Model model) {
+        if (product.getProductQuantity() < 1) {
+            model.addAttribute("error", "Quantity cannot be zero or negative!");
+            model.addAttribute("product", product);
+            return "createProduct";
+        }
         service.create(product);
         return "redirect:list";
     }
